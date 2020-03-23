8aa27235cb97f77fc615b3e8f5b8d8011203f9b5
Leonardo Salvucci <le.salvucci@gmail.com>
2020-03-23 14:02:47 -0300

    ajuste de scripts para utilizar las variables de entorno en los archivos .env y la capacidad de arrancar el server en modo development o modo produccion

--- server/wait-for-mongo.sh
+++ server/wait-for-mongo.sh
@@ -2,12 +2,35 @@
 
 : ${MONGO_HOST:=mongo}
 : ${MONGO_PORT:=27017}
-npm install
+FILE=.env
+if test -f "$FILE"
+then
+    export $(cat $FILE)
+else
+    echo "File .env not found. Using default configuration."
+fi
 
 until nc -z $MONGO_HOST $MONGO_PORT
 do
     echo "Waiting for Mongo ($MONGO_HOST:$MONGO_PORT) to start..."
     sleep 0.5
+
 done
+
+if [ -z "$MODE" ]
+then
+    : ${MODE:=development}
+fi
+
+if [ $MODE = "development" ]
+then
+    echo "Running in development mode"
+    npm install
+    npm run dev
+else
+    echo "Running in production mode"
+    npm install --only=prod
+    npm run start
+fi
 
 eval $*
