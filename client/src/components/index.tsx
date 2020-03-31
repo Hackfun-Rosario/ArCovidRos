9770877868a5b3bc6ebf64ad852d7b4c3667d22a
Maximiliano Tulian <maxtulian93@gmail.com>
2020-03-31 00:24:00 -0300

    Fix indentation

--- client/src/components/index.tsx
+++ client/src/components/index.tsx
@@ -1,17 +1,18 @@
 import React, { ReactElement } from 'react';
-import { useLocation,useHistory } from 'react-router-dom';
+import { useLocation, useHistory } from 'react-router-dom';
 
 import Abm from './Abm/index';
 import Login from './Login/index';
 import Layout from './Layout/index';
 import Home from './Home/index';
-import {Button, Typography } from '@material-ui/core';
+import { Button, Typography } from '@material-ui/core';
 
 import { constants, session } from 'utils';
 
 interface HomeProps {
   children: any;
 }
+
 const NoMatch = (): ReactElement => {
   const location = useLocation();
 
@@ -20,24 +21,19 @@ const NoMatch = (): ReactElement => {
 
   const history = useHistory();
 
-
   const handleLinkClick = (path: string): void => {
     history.push(path);
   };
 
-
   return (
-      <div style={{height:"100vh",display:"flex",alignItems:"center",justifyContent:"center"}} >
-        <div style={{padding:"150px 50px 150px 50px", borderRadius:"10px",boxShadow:"2px 2px 10px #666 ",textAlign:"center"}}>
+      <div style={{ height: "100vh", display: "flex", alignItems: "center", justifyContent: "center" }}>
+        <div style={{ padding: "150px 50px 150px 50px", borderRadius: "10px", boxShadow: "2px 2px 10px #666 ", textAlign: "center" }}>
         <Typography variant="h6">La ruta <code>{location.pathname}</code> es inválida</Typography>
-        
         <Button color="primary" variant="contained" onClick={() => handleLinkClick(ROUTES.HOME)}>
           Volver al inicio
         </Button>
-        </div>
-          </div>
-    
-    
+      </div>
+    </div>
   );
 };
 
