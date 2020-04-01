94d1ed1afc6a80f9b44afaa5014f2d43b23ab07a
Ale Piumetti <alepiumetti@hotmail.com>
2020-04-01 16:17:02 -0300

    Cambio de appbar de Layout a Header

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -1,14 +1,135 @@
-import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Button from '@material-ui/core/Button';
+import React from "react";
+import { useHistory } from "react-router-dom";
+
+import {
+  Button,
+  Grid,
+  Link,
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  MenuItem,
+  Menu,
+} from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import HackFunIcon from "../Icons/Ico01.svg";
+import { constants, session } from "utils";
+import Dialog from "../Dialog/index";
 
 export default function Header() {
+  const { isAuthorized, clearStorage } = session;
+  const { ROUTES } = constants;
+
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+  const history = useHistory();
+
+  const logOut = (): void => {
+    clearStorage();
+    history.push(ROUTES.LOGIN);
+  };
+  const handleLinkClick = (path: string): void => {
+    history.push(path);
+  };
+
+  const handleMenuClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <div>
-      <AppBar position="static">
-        <Toolbar>
-          <Button color="inherit">Cerrar sesión</Button>
+      <AppBar style={{ background: "#00011A" }} position="static">
+        <Toolbar variant="regular">
+          <Grid container direction="row" alignItems="center">
+            <Grid style={{ margin: "0 15px" }}>
+              <img src={HackFunIcon} width="60px" />
+            </Grid>
+            <Grid xs={10}>
+              <Typography variant="h6">HackFun</Typography>
+            </Grid>
+            <Grid>
+              <IconButton edge="end" color="inherit" aria-label="github">
+                <a
+                  style={{ color: "white" }}
+                  href="https://github.com/Hackfun-Rosario/ArCovidRos"
+                >
+                  <GitHubIcon />
+                </a>
+              </IconButton>
+            </Grid>
+            <Grid>
+              <IconButton
+                style={{ marginLeft: "15px" }}
+                edge="end"
+                color="inherit"
+                aria-label="menu"
+                onClick={handleMenuClick}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="simple-menu"
+                anchorEl={anchorEl}
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={handleMenuClose}
+              >
+                <MenuItem onClick={handleMenuClose}>
+                  <Link
+                    style={{
+                      marginRight: "10px",
+                      textDecoration: "none",
+                      color: "black",
+                    }}
+                    onClick={() => handleLinkClick(ROUTES.HOME)}
+                  >
+                    Inicio
+                  </Link>
+                </MenuItem>
+                {isAuthorized() && (
+                  <div>
+                    <MenuItem onClick={handleMenuClose}>
+                      <Link
+                        style={{
+                          marginRight: "10px",
+                          textDecoration: "none",
+                          color: "black",
+                        }}
+                        onClick={() => handleLinkClick(ROUTES.ABM_PROVINCE)}
+                      >
+                        Alta Provincia
+                      </Link>
+                    </MenuItem>
+                    <MenuItem onClick={handleMenuClose}>
+                      <Link
+                        onClick={() => handleLinkClick(ROUTES.ABM_CITY)}
+                        style={{ textDecoration: "none", color: "black" }}
+                      >
+                        Alta Ciudad
+                      </Link>
+                    </MenuItem>
+                    <MenuItem onClick={logOut}>Logout</MenuItem>
+                  </div>
+                )}
+                {!isAuthorized() && (
+                  <MenuItem onClick={handleMenuClose}>
+                    <Link
+                      onClick={() => handleLinkClick(ROUTES.LOGIN)}
+                      style={{ textDecoration: "none", color: "black" }}
+                    >
+                      Login
+                    </Link>
+                  </MenuItem>
+                )}
+              </Menu>
+            </Grid>
+          </Grid>
         </Toolbar>
       </AppBar>
     </div>
--- client/src/components/Layout/index.tsx
+++ client/src/components/Layout/index.tsx
@@ -1,19 +1,6 @@
 import React, { ReactElement } from "react";
 import { useHistory } from "react-router-dom";
-import {
-  Button,
-  Grid,
-  Link,
-  AppBar,
-  Toolbar,
-  IconButton,
-  Typography,
-  MenuItem,
-  Menu,
-} from "@material-ui/core";
-import MenuIcon from "@material-ui/icons/Menu";
-import GitHubIcon from "@material-ui/icons/GitHub";
-import HackFunIcon from "../Icons/Ico01.svg";
+import Header from "../Header/index";
 
 import cn from "classnames";
 
@@ -25,119 +12,9 @@ interface LayoutProps {
 }
 
 const Layout = ({ children }: LayoutProps): ReactElement => {
-  const { isAuthorized, clearStorage } = session;
-  const { ROUTES } = constants;
-
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-
-  const history = useHistory();
-
-  const logOut = (): void => {
-    clearStorage();
-    history.push(ROUTES.LOGIN);
-  };
-  const handleLinkClick = (path: string): void => {
-    history.push(path);
-  };
-
-  const handleMenuClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleMenuClose = () => {
-    setAnchorEl(null);
-  };
-
   return (
     <div>
-      <AppBar style={{ background: "#00011A" }} position="static">
-        <Toolbar variant="regular">
-          <Grid container direction="row" alignItems="center">
-            <Grid style={{ margin: "0 15px" }}>
-              <img src={HackFunIcon} width="60px" />
-            </Grid>
-            <Grid xs={10}>
-              <Typography variant="h6">HackFun</Typography>
-            </Grid>
-            <Grid>
-              <IconButton edge="end" color="inherit" aria-label="menu">
-                <a
-                  style={{ color: "white" }}
-                  href="https://github.com/Hackfun-Rosario/ArCovidRos"
-                >
-                  <GitHubIcon />
-                </a>
-              </IconButton>
-            </Grid>
-            <Grid>
-              <IconButton
-                style={{ marginLeft: "15px" }}
-                edge="end"
-                color="inherit"
-                aria-label="menu"
-                onClick={handleMenuClick}
-              >
-                <MenuIcon />
-              </IconButton>
-              <Menu
-                id="simple-menu"
-                anchorEl={anchorEl}
-                keepMounted
-                open={Boolean(anchorEl)}
-                onClose={handleMenuClose}
-              >
-                <MenuItem onClick={handleMenuClose}>
-                  <Link
-                    style={{
-                      marginRight: "10px",
-                      textDecoration: "none",
-                      color: "black",
-                    }}
-                    onClick={() => handleLinkClick(ROUTES.HOME)}
-                  >
-                    Inicio
-                  </Link>
-                </MenuItem>
-                {isAuthorized() && (
-                  <div>
-                    <MenuItem onClick={handleMenuClose}>
-                      <Link
-                        style={{
-                          marginRight: "10px",
-                          textDecoration: "none",
-                          color: "black",
-                        }}
-                        onClick={() => handleLinkClick(ROUTES.ABM_PROVINCE)}
-                      >
-                        Alta Provincia
-                      </Link>
-                    </MenuItem>
-                    <MenuItem onClick={handleMenuClose}>
-                      <Link
-                        onClick={() => handleLinkClick(ROUTES.ABM_CITY)}
-                        style={{ textDecoration: "none", color: "black" }}
-                      >
-                        Alta Ciudad
-                      </Link>
-                    </MenuItem>
-                    <MenuItem onClick={logOut}>Logout</MenuItem>
-                  </div>
-                )}
-                {!isAuthorized() && (
-                  <MenuItem onClick={handleMenuClose}>
-                    <Link
-                      onClick={() => handleLinkClick(ROUTES.LOGIN)}
-                      style={{ textDecoration: "none", color: "black" }}
-                    >
-                      Login
-                    </Link>
-                  </MenuItem>
-                )}
-              </Menu>
-            </Grid>
-          </Grid>
-        </Toolbar>
-      </AppBar>
+      <Header />
       <div>{children}</div>
     </div>
   );
