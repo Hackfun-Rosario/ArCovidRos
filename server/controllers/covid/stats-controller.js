9449aec4d7cb07401bc702fe95e029c853b82aab
Mijail Todorovich <mijailtodorovich+git@gmail.com>
2020-04-07 18:08:12 -0300

    Agrego parámetro limit
    Closes #21
    
    Teniendo limit variable, tuve que hacer más robusto el manejo de errores

--- server/controllers/covid/stats-controller.js
+++ server/controllers/covid/stats-controller.js
@@ -1,7 +1,6 @@
 const CovidStats = require('../../models/covid/stats-model'),
   responses = require('../../responses')
 
-const limit = 25; // page elements
 const avoidables = {
     _id: 0,
     __v: 0,
@@ -10,23 +9,29 @@ const avoidables = {
     updatedAt: 0
   }; // data we don't render
 
-function getStats(req, res, filter) {
-  // normalize page value
-  let page = Math.abs(req.params.page) || 0
+async function getStats(req, res, filter) {
 
-    queryResultPromise = CovidStats.find(filter, avoidables)
+  try {
+    let page  = req.params.page != undefined ? parseInt(req.params.page) : 0
+    let limit = req.query.limit != undefined ? parseInt(req.query.limit) : 25
+
+    if (page  < 0 || isNaN(page))  { throw new RangeError("Page has to be a number greater than or equal to zero." ) }
+    if (limit < 0 || isNaN(limit)) { throw new RangeError("Limit has to be a number greater than or equal to zero.") }
+
+    query = CovidStats.find(filter, avoidables)
       .sort({ fecha: -1 })
       .skip(page * limit)
-      .limit(limit)
-      .exec()
-      .then(stats => {
-        return res.status(200).json(responses.responseData(stats))
-      })
-      .catch(err => {
-        return res.status(400).json(responses.responseError(err))
-      })
 
-  return queryResultPromise
+    if (limit > 0) {
+      query = query.limit(limit)
+    }
+
+    stats = await query.exec()
+    res.status(200).json(responses.responseData(stats))
+  }
+  catch (err) {
+    res.status(400).json(responses.responseError(err.toString()))
+  }
 }
 
 const createStat = async (req, res) => {
