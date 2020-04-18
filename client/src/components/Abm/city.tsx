6bcb9cd5943bd842768b17d23b08fefa47503d3e
Ale Piumetti <alepiumetti@hotmail.com>
2020-04-18 17:18:03 -0300

    corrección de formulario + variables

--- client/src/components/Abm/city.tsx
+++ client/src/components/Abm/city.tsx
@@ -1,10 +1,21 @@
-import axios from 'axios';
-import React, { useState } from 'react';
-import { Button, Grid, TextField, Typography } from '@material-ui/core';
-import moment from 'moment';
+import axios from "axios";
+import React, { useState } from "react";
+import {
+  Button,
+  Grid,
+  TextField,
+  Typography,
+  Divider,
+  FormControl,
+  Select,
+  InputLabel,
+  MenuItem,
+  FormHelperText,
+} from "@material-ui/core";
+import moment from "moment";
 
-import { METHODS, ENDPOINTS } from 'utils/constants';
-import { Layout } from 'components';
+import { METHODS, ENDPOINTS } from "utils/constants";
+import { Layout } from "components";
 
 const Abm = () => {
   const [loading, setLoading] = useState<boolean>(false);
@@ -15,29 +26,27 @@ const Abm = () => {
     await axios({
       method: METHODS.POST,
       url: ENDPOINTS.ABM,
-      data: {
-
-      }
-    }).then((response) => {
-
-    }).catch((error) => {
-      console.warn('Error while tryng to log in', error);
-    });
+      data: {},
+    })
+      .then((response) => {})
+      .catch((error) => {
+        console.warn("Error while tryng to log in", error);
+      });
 
     setLoading(false);
   };
 
   const [date, setDate] = useState<date>(null);
-  const [city, setCity] = useState<string>('');
-  const [department, setDepartment] = useState<string>('');
-  const [zone, setZone] = useState<string>('');
+  const [city, setCity] = useState<string>("");
+  const [department, setDepartment] = useState<string>("");
+  const [zone, setZone] = useState<string>("");
   const [cases, setCases] = useState<number>(null);
   const [newCases, setNewCases] = useState<number>(null);
   const [deads, setDeads] = useState<number>(null);
   const [newDeads, setNewDeads] = useState<number>(null);
   const [recover, setRecover] = useState<number>(null);
   const [newRecover, setNewRecover] = useState<number>(null);
-  const [url, setUrl] = useState<string>('');
+  const [url, setUrl] = useState<string>("");
 
   return (
     <Layout>
@@ -48,56 +57,219 @@ const Abm = () => {
 
         <Grid item xs={6}>
           <Grid item>
-            <TextField fullWidth label="Día" required
-            value={date} onChange={e => setDate(e?.target?.value)} />
+            <TextField
+              fullWidth
+              label="Día"
+              required
+              value={date}
+              onChange={(e) => setDate(e?.target?.value)}
+            />
           </Grid>
           <Grid item>
-            <TextField fullWidth label="Ciudad"
-              value={city} onChange={e => setCity(e?.target?.value)} />
+            <TextField
+              fullWidth
+              label="Ciudad"
+              value={city}
+              onChange={(e) => setCity(e?.target?.value)}
+            />
           </Grid>
           <Grid item>
-            <TextField fullWidth label="Total de confirmados" required
-              value={cases} onChange={e => setCases(e?.target?.value)} />
+            <TextField
+              fullWidth
+              label="Total de confirmados"
+              required
+              value={cases}
+              onChange={(e) => setCases(e?.target?.value)}
+            />
           </Grid>
           <Grid item>
-            <TextField fullWidth label="Total de muertos"
-              value={deads} onChange={e => setDeads(e?.target?.value)} />
+            <TextField
+              fullWidth
+              label="Total de muertos"
+              value={deads}
+              onChange={(e) => setDeads(e?.target?.value)}
+            />
           </Grid>
           <Grid item>
-            <TextField fullWidth label="Total de recuperados" required
-              value={recover} onChange={e => setRecover(e?.target?.value)} />
+            <TextField
+              fullWidth
+              label="Total de recuperados"
+              required
+              value={recover}
+              onChange={(e) => setRecover(e?.target?.value)}
+            />
           </Grid>
           <Grid item>
-            <TextField fullWidth label="Url" required
-              value={url} onChange={e => setUrl(e?.target?.value)} />
+            <TextField
+              fullWidth
+              label="Url"
+              required
+              value={url}
+              onChange={(e) => setUrl(e?.target?.value)}
+            />
           </Grid>
         </Grid>
 
         <Grid item xs={6}>
           <Grid item>
-            <TextField fullWidth label="Departamento"
-              value={department} onChange={e => setDepartment(e?.target?.value)} />
+            <TextField
+              fullWidth
+              label="Departamento"
+              value={department}
+              onChange={(e) => setDepartment(e?.target?.value)}
+            />
           </Grid>
           <Grid item>
-            <TextField fullWidth label="Zona"
-              value={zone} onChange={e => setZone(e?.target?.value)} />
+            <TextField
+              fullWidth
+              label="Zona"
+              value={zone}
+              onChange={(e) => setZone(e?.target?.value)}
+            />
           </Grid>
           <Grid item>
-            <TextField fullWidth label="Nuevos casos" required
-              value={newCases} onChange={e => setNewCases(e?.target?.value)} />
+            <TextField
+              fullWidth
+              label="Nuevos casos"
+              required
+              value={newCases}
+              onChange={(e) => setNewCases(e?.target?.value)}
+            />
           </Grid>
           <Grid item>
-            <TextField fullWidth label="Nuevos muertos"
-              value={newDeads} onChange={e => setNewDeads(e?.target?.value)} />
+            <TextField
+              fullWidth
+              label="Nuevos muertos"
+              value={newDeads}
+              onChange={(e) => setNewDeads(e?.target?.value)}
+            />
           </Grid>
           <Grid item>
-            <TextField fullWidth label="Nuevos recuperados" required
-              value={newRecover} onChange={e => setNewRecover(e?.target?.value)} />
+            <TextField
+              fullWidth
+              label="Nuevos recuperados"
+              required
+              value={newRecover}
+              onChange={(e) => setNewRecover(e?.target?.value)}
+            />
           </Grid>
         </Grid>
 
         <Grid item xs={12} align="right">
-          <Button color="primary" disabled={loading} variant="contained" onClick={handleClick}>Agregar</Button>
+          <Button
+            color="primary"
+            disabled={loading}
+            variant="contained"
+            onClick={handleClick}
+          >
+            Agregar
+          </Button>
+        </Grid>
+      </Grid>
+      <br />
+      <Divider variant="middle" />
+
+      <Grid container justify="center" direction="row" spacing={4}>
+        <Grid item xs={3}>
+          <Typography variant="h5">
+            Actualización de casos por Ciudad
+          </Typography>
+        </Grid>
+
+        <Grid item xs={3}>
+          <FormControl>
+            <InputLabel id="elect-fecha">Fecha</InputLabel>
+            <Select
+              style={{ minWidth: "120" }}
+              labelId="select-fecha"
+              id="select-fecha"
+              displayEmpty
+            >
+              <MenuItem disabled value="">
+                Fecha
+              </MenuItem>
+            </Select>
+            <FormHelperText>Seleccionar Fecha para actualizar</FormHelperText>
+          </FormControl>
+        </Grid>
+
+        <Grid item xs={3}>
+          <FormControl>
+            <InputLabel id="elect-prov">Provincia</InputLabel>
+            <Select
+              style={{ minWidth: "120" }}
+              labelId="select-prov"
+              id="select-prov"
+              displayEmpty
+            >
+              <MenuItem disabled value="">
+                Provincia
+              </MenuItem>
+            </Select>
+            <FormHelperText>Seleccionar Provincia</FormHelperText>
+          </FormControl>
+        </Grid>
+
+        <Grid item xs={3}>
+          <FormControl>
+            <InputLabel id="elect-city">Ciudad</InputLabel>
+            <Select
+              style={{ minWidth: "120" }}
+              labelId="select-city"
+              id="select-city"
+              displayEmpty
+            >
+              <MenuItem disabled value="">
+                Ciudad
+              </MenuItem>
+            </Select>
+            <FormHelperText>Seleccionar una ciudad</FormHelperText>
+          </FormControl>
+        </Grid>
+
+        <Grid item xs={6}>
+          <Grid item>
+            <TextField fullWidth label="Día" required />
+          </Grid>
+          <Grid item>
+            <TextField fullWidth label="Ciudad" />
+          </Grid>
+          <Grid item>
+            <TextField fullWidth label="Total de confirmados" required />
+          </Grid>
+          <Grid item>
+            <TextField fullWidth label="Total de muertos" />
+          </Grid>
+          <Grid item>
+            <TextField fullWidth label="Total de recuperados" required />
+          </Grid>
+          <Grid item>
+            <TextField fullWidth label="Url" required />
+          </Grid>
+        </Grid>
+
+        <Grid item xs={6}>
+          <Grid item>
+            <TextField fullWidth label="Departamento" />
+          </Grid>
+          <Grid item>
+            <TextField fullWidth label="Zona" />
+          </Grid>
+          <Grid item>
+            <TextField fullWidth label="Nuevos casos" required />
+          </Grid>
+          <Grid item>
+            <TextField fullWidth label="Nuevos muertos" />
+          </Grid>
+          <Grid item>
+            <TextField fullWidth label="Nuevos recuperados" required />
+          </Grid>
+        </Grid>
+
+        <Grid item xs={12} align="right">
+          <Button color="primary" disabled={loading} variant="contained">
+            Actualizar
+          </Button>
         </Grid>
       </Grid>
     </Layout>
--- client/src/components/Abm/province.tsx
+++ client/src/components/Abm/province.tsx
@@ -1,10 +1,21 @@
-import axios from 'axios';
-import React, { useState } from 'react';
-import { Button, Grid, TextField, Typography } from '@material-ui/core';
-import moment from 'moment';
+import axios from "axios";
+import React, { useState } from "react";
+import {
+  Button,
+  Grid,
+  TextField,
+  Typography,
+  Divider,
+  Select,
+  InputLabel,
+  MenuItem,
+  FormControl,
+  FormHelperText,
+} from "@material-ui/core";
+import moment from "moment";
 
-import { METHODS, ENDPOINTS } from 'utils/constants';
-import { Layout } from 'components';
+import { METHODS, ENDPOINTS } from "utils/constants";
+import { Layout } from "components";
 
 const Abm = () => {
   const [loading, setLoading] = useState<boolean>(false);
@@ -15,27 +26,37 @@ const Abm = () => {
     await axios({
       method: METHODS.POST,
       url: ENDPOINTS.ABM,
-      data: {
-
-      }
-    }).then((response) => {
-
-    }).catch((error) => {
-      console.warn('Error while tryng to log in', error);
-    });
+      data: {},
+    })
+      .then((response) => {})
+      .catch((error) => {
+        console.warn("Error while tryng to log in", error);
+      });
 
     setLoading(false);
   };
 
   const [date, setDate] = useState<date>(null);
-  const [province, setProvince] = useState<string>('');
+  const [province, setProvince] = useState<string>("");
   const [cases, setCases] = useState<number>(null);
   const [newCases, setNewCases] = useState<number>(null);
   const [deads, setDeads] = useState<number>(null);
   const [newDeads, setNewDeads] = useState<number>(null);
   const [recover, setRecover] = useState<number>(null);
   const [newRecover, setNewRecover] = useState<number>(null);
-  const [url, setUrl] = useState<string>('');
+  const [url, setUrl] = useState<string>("");
+
+  //Campos para editar
+
+  const [dateEdit, setDateEdit] = useState<date>(null);
+  const [provinceEdit, setProvinceEdit] = useState<string>("");
+  const [casesEdit, setCasesEdit] = useState<number>(null);
+  const [newCasesEdit, setNewCasesEdit] = useState<number>(null);
+  const [deadsEdit, setDeadsEdit] = useState<number>(null);
+  const [newDeadsEdit, setNewDeadsEdit] = useState<number>(null);
+  const [recoverEdit, setRecoverEdit] = useState<number>(null);
+  const [newRecoverEdit, setNewRecoverEdit] = useState<number>(null);
+  const [urlEdit, setUrlEdit] = useState<string>("");
 
   return (
     <Layout>
@@ -46,50 +67,228 @@ const Abm = () => {
 
         <Grid item xs={6}>
           <Grid item>
-            <TextField fullWidth label="Día" required
-              value={date} onChange={e => setDate(e?.target?.value)} />
+            <TextField
+              fullWidth
+              label="Día"
+              required
+              value={date}
+              onChange={(e) => setDate(e?.target?.value)}
+            />
           </Grid>
           <Grid item>
-            <TextField fullWidth label="Total de confirmados" required
-              value={cases} onChange={e => setCases(e?.target?.value)} />
+            <TextField
+              fullWidth
+              label="Total de confirmados"
+              required
+              value={cases}
+              onChange={(e) => setCases(e?.target?.value)}
+            />
           </Grid>
           <Grid item>
-            <TextField fullWidth label="Total de muertos"
-              value={deads} onChange={e => setDeads(e?.target?.value)} />
+            <TextField
+              fullWidth
+              label="Total de muertos"
+              value={deads}
+              onChange={(e) => setDeads(e?.target?.value)}
+            />
           </Grid>
           <Grid item>
-            <TextField fullWidth label="Total de recuperados" required
-              value={recover} onChange={e => setRecover(e?.target?.value)} />
+            <TextField
+              fullWidth
+              label="Total de recuperados"
+              required
+              value={recover}
+              onChange={(e) => setRecover(e?.target?.value)}
+            />
           </Grid>
           <Grid item>
-            <TextField fullWidth label="Url" required
-              value={url} onChange={e => setUrl(e?.target?.value)} />
+            <TextField
+              fullWidth
+              label="Url"
+              required
+              value={url}
+              onChange={(e) => setUrl(e?.target?.value)}
+            />
           </Grid>
         </Grid>
 
         <Grid item xs={6}>
           <Grid item>
-            <TextField fullWidth label="Provincia" required
-              value={province} onChange={e => setProvince(e?.target?.value)} />
+            <TextField
+              fullWidth
+              label="Provincia"
+              required
+              value={province}
+              onChange={(e) => setProvince(e?.target?.value)}
+            />
           </Grid>
           <Grid item>
-            <TextField fullWidth label="Nuevos casos"
-              value={newCases} onChange={e => setNewCases(e?.target?.value)} />
+            <TextField
+              fullWidth
+              label="Nuevos casos"
+              value={newCases}
+              onChange={(e) => setNewCases(e?.target?.value)}
+            />
           </Grid>
 
           <Grid item>
-            <TextField fullWidth label="Nuevos muertos"
-              value={newDeads} onChange={e => setNewDeads(e?.target?.value)} />
+            <TextField
+              fullWidth
+              label="Nuevos muertos"
+              value={newDeads}
+              onChange={(e) => setNewDeads(e?.target?.value)}
+            />
           </Grid>
 
           <Grid item>
-            <TextField fullWidth label="Nuevos recuperados"
-              value={newRecover} onChange={e => setNewRecover(e?.target?.value)} />
+            <TextField
+              fullWidth
+              label="Nuevos recuperados"
+              value={newRecover}
+              onChange={(e) => setNewRecover(e?.target?.value)}
+            />
           </Grid>
         </Grid>
 
-                <Grid item xs={12} align="right">
-          <Button color="primary" disabled={loading} variant="contained" onClick={handleClick}>Agregar</Button>
+        <Grid item xs={12} align="right">
+          <Button
+            color="primary"
+            disabled={loading}
+            variant="contained"
+            onClick={handleClick}
+          >
+            Agregar
+          </Button>
+        </Grid>
+        <br />
+        <Divider variant="middle" />
+
+        <Grid item xs={3}>
+          <Typography variant="h5">
+            Actualización de casos por Provincia
+          </Typography>
+        </Grid>
+        <Grid item xs={3}>
+          <FormControl>
+            <InputLabel id="elect-fecha">Fecha</InputLabel>
+            <Select
+              style={{ minWidth: "120" }}
+              labelId="select-fecha"
+              id="select-fecha"
+              displayEmpty
+            >
+              <MenuItem disabled value="">
+                Fecha
+              </MenuItem>
+            </Select>
+            <FormHelperText>
+              Seleccionar una fecha para actualizar
+            </FormHelperText>
+          </FormControl>
+        </Grid>
+
+        <Grid item xs={3}>
+          <FormControl>
+            <InputLabel id="elect-prov">Provincia</InputLabel>
+            <Select
+              style={{ minWidth: "120" }}
+              labelId="select-prov"
+              id="select-prov"
+              displayEmpty
+            >
+              <MenuItem disabled value="">
+                Provincia
+              </MenuItem>
+              {}
+            </Select>
+            <FormHelperText>Seleccionar Provincia</FormHelperText>
+          </FormControl>
+        </Grid>
+
+        <Grid item xs={6}>
+          <Grid item>
+            <TextField fullWidth label="Día" required value={dateEdit} />
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              fullWidth
+              label="Total de confirmados"
+              required
+              value={casesEdit}
+              onChange={(e) => setCasesEdit(e?.target?.value)}
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              fullWidth
+              label="Total de muertos"
+              value={deadsEdit}
+              onChange={(e) => setDeadsEdit(e?.target?.value)}
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              fullWidth
+              label="Total de recuperados"
+              required
+              value={recoverEdit}
+              onChange={(e) => setRecoverEdit(e?.target?.value)}
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              fullWidth
+              label="Url"
+              required
+              value={urlEdit}
+              onChange={(e) => setUrlEdit(e?.target?.value)}
+            />
+          </Grid>
+        </Grid>
+
+        <Grid item xs={6}>
+          <Grid item>
+            <TextField
+              fullWidth
+              label="Provincia"
+              required
+              disabled
+              value={provinceEdit}
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              fullWidth
+              label="Nuevos casos"
+              value={newCasesEdit}
+              onChange={(e) => setNewCasesEdit(e?.target?.value)}
+            />
+          </Grid>
+
+          <Grid item>
+            <TextField
+              fullWidth
+              label="Nuevos muertos"
+              value={newDeadsEdit}
+              onChange={(e) => setNewDeadsEdit(e?.target?.value)}
+            />
+          </Grid>
+
+          <Grid item>
+            <TextField
+              fullWidth
+              label="Nuevos recuperados"
+              value={newRecoverEdit}
+              onChange={(e) => setNewRecoverEdit(e?.target?.value)}
+            />
+          </Grid>
+        </Grid>
+
+        <Grid item xs={12} align="right">
+          <Button color="primary" disabled={loading} variant="contained">
+            Actualizar
+          </Button>
         </Grid>
       </Grid>
     </Layout>
